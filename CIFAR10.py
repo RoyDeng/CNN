4f8064efd08fbe0c74a5c313fb55453a4df250fe
Yu-Teng Deng <xoxo4795946@yahoo.com.tw>
2018-06-15 08:46:52 +0800

    modify model & add tensorboard

--- CIFAR10.py
+++ CIFAR10.py
@@ -1,54 +1,74 @@
-# 載入資料集
-import numpy as np
-np.random.seed(10)
+# 匯入相關模組
+import numpy
+import matplotlib.pyplot as plt
 from keras.datasets import cifar10
-(x_train_image, y_train_label), (x_test_image, y_test_label)=cifar10.load_data()
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.constraints import maxnorm
+from keras.optimizers import SGD
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.utils import np_utils
+from keras import backend as K
+from keras.callbacks import TensorBoard
+K.set_image_dim_ordering('th')
 
-# 顯示資料集的結構
-x_train_image.shape
-x_test_image.shape
-y_train_label.shape
-y_test_label.shape
+# 初始化隨機數種子，確保結果是複製的
+seed = 7
+numpy.random.seed(seed)
+
+# 載入資料集
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
 
 # 資料預處理
-# 數字圖片
-x_train_normalize=x_train_image.astype('float32')/255.0
-x_test_normalize=x_test_image.astype('float32')/255.0
+# 載入的資料型態是整數，必須轉換成浮點數才能執行除法
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train = X_train / 255.0
+X_test = X_test / 255.0
 
-# 標籤
-from keras.utils import np_utils
-y_train_onehot=np_utils.to_categorical(y_train_label)
-y_test_onehot=np_utils.to_categorical(y_test_label)
+# 將各類資料定義為 0 ~ 1 的整數向量，以便對分類問題進行最佳建模，預期此二進制矩陣寬度為 10
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
 
 # 建立模型
-# 匯入相關模組
-from keras.models import Sequential
-from keras.layers import Dense,Dropout,Flatten,Conv2D,MaxPooling2D
-from keras.layers import ZeroPadding2D,Activation
-
-# 建立線性堆疊模型 : 卷積層 (丟棄 25% 神經元) * 2 + 池化層
+# 建立線性堆疊模型 : 輸入層 + 卷積層 * 6 + 池化層
 model=Sequential()
-model.add(Conv2D(filters=32, kernel_size=(3,3), padding='same', input_shape=(32,32,3), activation='relu'))
+model.add(Conv2D(32, (3, 3), input_shape=(3, 32, 32), activation='relu', padding='same'))
+model.add(Dropout(0.2))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(Dropout(0.2))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(Dropout(0.2))
+model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Dropout(0.25))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Conv2D(filters=64, kernel_size=(3,3), padding='same', activation='relu'))
-model.add(Dropout(0.25))
-model.add(MaxPooling2D(pool_size=(2,2)))
-
-# 建立分類模型 (MLP) : 平坦層 + 隱藏層 (1024 神經元) + 輸出層 (10 神經元)
+# 建立分類模型 (MLP) : 平坦層 + 完全連接層 (1024 神經元) + 完全連接層 (512 神經元) + 完全連接輸出層 (10 神經元)
 model.add(Flatten())
-model.add(Dropout(0.25))
-model.add(Dense(1024,activation='relu'))
-model.add(Dropout(0.25))
-model.add(Dense(10,activation='softmax'))
+model.add(Dropout(0.2))
+model.add(Dense(1024, activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+model.add(Dense(512, activation='relu', kernel_constraint=maxnorm(3)))
+model.add(Dropout(0.2))
+model.add(Dense(num_classes, activation='softmax'))
+
+# 編譯與訓練模型
+epochs = 25
+lrate = 0.01
+decay = lrate/epochs
+sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+train_history=model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size=64, callbacks=[TensorBoard(log_dir='./tmp/log')])
 
 # 檢視模型摘要
 print(model.summary())
-
-# 編譯與訓練模型
-model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-train_history=model.fit(x=x_train_normalize, y=y_train_onehot, validation_split=0.2, epochs=10, batch_size=128,verbose=2)
 
 # 繪製訓練結果
 def show_train_history(train_history):
@@ -70,20 +90,9 @@ def show_train_history(train_history):
     plt.legend(["train", "validation"], loc="upper left")
     plt.show()
 
-import matplotlib.pyplot as plt
 show_train_history(train_history)
 
 # 評估預測準確率
-scores=model.evaluate(x_test_normalize, y_test_onehot)
+scores=model.evaluate(X_test, y_test)
 print("Accuracy=", scores)
 print("Accuracy=", scores[1])
-
-# 預測測試集圖片
-prediction=model.predict_classes(x_test_normalize)
-print(prediction)
-print(prediction[:10])
-print(y_test_label[:10])
-
-# 製作混淆矩陣
-import pandas as pd
-pd.crosstab(y_test_label.reshape(-1), prediction, rownames=['label'],colnames=['predict'])
